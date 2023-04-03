f8053a40d18383684db9b1e8cef2872c0da0b282
Bharath Radhakrishnan <bradhakrishnan@dons.usfca.edu>
2023-04-02 23:24:48 -0700

    Create ExtraCredit_8_Trees_Q2.java
    Solution for Question_2 Extra Credit 8 - Trees.

---
+++ ExtraCredit_8_Trees_Q2.java
@@ -0,0 +1,21 @@
+// LeetCode Easy
+// 2331. Evaluate Boolean Binary Tree
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/
+
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val == 0){
+            return false;
+        }
+        if(root.val == 1){
+            return true;
+        }
+        if(root.val == 2){
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        if(root.val == 3){
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+        return false;
+    }
+}

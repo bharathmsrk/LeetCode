d546121410ca0140d1c08d9f8acfb47869755fe2
Bharath Radhakrishnan <bradhakrishnan@dons.usfca.edu>
2023-04-02 23:42:23 -0700

    Create ExtraCredit_8_Trees_Q3.java
    Solution for Question_3 Extra Credit 8 - Trees.

---
+++ ExtraCredit_8_Trees_Q3.java
@@ -0,0 +1,18 @@
+// LeetCode Easy
+// 104. Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        if (leftDepth > rightDepth){
+            return leftDepth + 1;
+        } else {
+            return rightDepth + 1;
+        }
+    }
+}
